8bc63c8ee5c2ab4409e097c626ac197c9e8a412a
Jeff Jacobson <jeffjacobson@MacbookPro.local>
2017-09-04 07:20:33 -0700

    initial add

---
+++ Arduino/BlinkWithTimer/BlinkWithTimer.ino
@@ -0,0 +1,21 @@
+/*
+ * A better blink without delay or running a process at intervals without a delay
+ */
+
+#include "Timer.h"  //http://github.com/JChristensen/Timer
+Timer t;            //instantiate the timer object
+
+void setup(void)
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+    t.oscillate(LED_BUILTIN, 1000, LOW); // sets up the time for 1 second intervals
+}
+
+void loop(void)
+{
+    // only requires a few milliseconds to run the timer update
+    t.update();
+
+    // other code here, runs without a delay() function above
+}
+
---
+++ Arduino/BlinkWithTimer/README.md
@@ -0,0 +1,5 @@
+## A simple test of the Timer library 
+
+Uses Jack Christensen's Timer [http://github.com/JChristensen/Timer]
+
+This library is useful for running code without a delay() in the main loop().
